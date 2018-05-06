94f4d8cb41fb2ac3781d047fdb75b55e6dfc1291
Dariem Pérez Herrera <dariemp@gmail.com>
2018-05-06 01:09:24 -0600

    Apply some DRY and dependency injection to improve code reusability

--- web.py
+++ web.py
@@ -1,36 +1,40 @@
-import json
-
 import os
-import click
 from flask import Flask, Response
 from lib import PalindromeFinder, JSONFileKeyReader
 
 
 app = Flask(__name__)
 
-def buid_response(palindromes):
+
+def _get_palindromes():
+    file_path = os.environ.get('FLASK_INPUT_FILE_PATH', 'sample.txt')
+    palindrome_finder = PalindromeFinder(JSONFileKeyReader(file_path))
+    return palindrome_finder.get_palindromes()
+
+
+def palindromes_list_builder(palindromes):
     response = ''
     for palindrome in palindromes:
         response += palindrome + '\n'
     return response
 
-@app.route('/palindromes')
-def get_palindromes():
-    file_path = os.environ.get('FLASK_INPUT_FILE_PATH', 'sample.txt')
+
+def palindromes_count_builder(palindromes):
+    return str(len(palindromes))
+
+
+def endpoint_handler(reponse_builder):
     try:
-        palindrome_finder = PalindromeFinder(JSONFileKeyReader(file_path))
-        palindromes = palindrome_finder.get_palindromes()
-        return Response(buid_response(palindromes), content_type='text/plain; utf-8')
+        return Response(reponse_builder(_get_palindromes()), content_type='text/plain; utf-8')
     except FileNotFoundError:
         return Response('Server improperly configured', status=500, content_type='text/plain; utf-8')
+
+
+@app.route('/palindromes')
+def get_palindromes():
+    return endpoint_handler(palindromes_list_builder)
 
 
 @app.route('/palindromes/count')
 def get_palindromes_count():
-    file_path = os.environ.get('FLASK_INPUT_FILE_PATH', 'sample.txt')
-    try:
-        palindrome_finder = PalindromeFinder(JSONFileKeyReader(file_path))
-        palindromes = palindrome_finder.get_palindromes()
-        return Response(str(len(palindromes)), content_type='text/plain; utf-8')
-    except FileNotFoundError:
-        return Response('Server improperly configured', status=500, content_type='text/plain; utf-8')
+    return endpoint_handler(palindromes_count_builder)
